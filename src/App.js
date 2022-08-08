fbf8b7f57b3b4fc82c4031f21ce93d70ddecc2a7
Julia Palichleb <juliapalichlebcc@gmail.com>
2022-08-08 17:06:51 +0200

    useeffect axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-
+import {useEffect, useState} from "react";
+import axios from "axios";
+import { isEmpty } from "lodash";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect( () => {
+    axios.get(`https://api.github.com/orgs/alibaba/repos?per_page=200`).then(({ data }) => setData(data));
+  }, [])
+
+
   return (
     <div className="App">
-
+      { !isEmpty(data) ? data[2].full_name : 'Loading...' }
     </div>
   );
 }
